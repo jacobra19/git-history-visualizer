c7e5add484913ec6dfa7d0ac435166d8217e861b
Yakov Rakhamimov <jacobra19@gmail.com>
2021-11-13 23:41:07 +0200

    added Commit interface

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+interface Commit {
+    status: string[];
+    files: string[];
+    hash: string;
+    abbrevHash: string;
+    treeHash: string;
+    abbrevTreeHash: string;
+    parentHashes: string;
+    abbrevParentHashes: string;
+    authorName: string;
+    authorEmail: string;
+    authorDate: string;
+    authorDateRel: string;
+    committerName: string;
+    committerEmail: string;
+    committerDate: string;
+    committerDateRel: string;
+    subject: string;
+    body: string;
+    rawBody: string;
+}
